87ee37906477229a377f5552e832234efa0eae3d
Rohit <prk304@gmail.com>
2018-06-24 18:08:23 +0530

    Command Line Arguments

---
+++ WrapperCls/Command.java
@@ -0,0 +1,15 @@
+
+public class Command {
+
+	public static void main(String[] args) {
+		int n=args.length;
+		System.out.println(n);
+		  
+		for(int i = 1;i<=n;i++) {
+			System.out.println(args[i]);
+			
+		}
+
+	}
+
+}
