284aad592eab3e1a6adaa5fd18fce2a9d7c9561f
Rohit <prk304@gmail.com>
2018-06-24 18:39:54 +0530

    Wrapper Class To Long

---
+++ WrapperCls/rohit/Wc.java
@@ -0,0 +1,19 @@
+package rohit;
+
+public class Wc {
+
+	public static void main(String[] args) 
+	{
+		String no="7894900150";
+		String n="P Rohit Kumar";
+		
+         long b=Long.parseLong(no);
+		
+		Long ob=Long.valueOf(b);
+		        
+	System.out.println(b);               
+		
+
+	}
+
+}
